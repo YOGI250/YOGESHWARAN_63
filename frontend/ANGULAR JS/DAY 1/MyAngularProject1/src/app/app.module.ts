9ed9a58b0f070580e24fa8976612f44e3831caac
YOGI250 <140896318+YOGI250@users.noreply.github.com>
2024-08-07 17:46:00 +0530

    sql notes

--- frontend/ANGULAR JS/DAY 1/MyAngularProject1/src/app/app.module.ts
+++ frontend/ANGULAR JS/DAY 1/MyAngularProject1/src/app/app.module.ts
@@ -4,11 +4,24 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { YogiComponent } from './yogi/yogi.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
+const ROUTES:Routes=[
+  { path:'home',component : HomeComponent},
+  { path:'home',component : HomeComponent},
+  { path:'home',component : HomeComponent}
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    YogiComponent
+    YogiComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
